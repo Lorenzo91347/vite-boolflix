f3ddceb69fbd9a35cf27cf35f99706f8658ac164
lorenzo91347 <lorenzo.losito91@gmail.com>
2023-12-15 09:35:01 +0100

    feat:milestone 1

--- src/components/FlixMovies.vue
+++ src/components/FlixMovies.vue
@@ -11,14 +11,19 @@ export default{
         console.log('created FlixMovies')
         axios.get(this.store.apiUrl)
         .then((response) => {
-            console.log(response);
-            this.store.movies = response.data.results
-        })
+            this.store.movies = response.data
+        });
+        console.log(this.store.movies)
     }
 }
 
 </script>
 <template>
-
+<ul v-for="item in store.movies.results">
+    <li>{{ item.original_title }}</li>
+    <li>{{ item.title }}</li>
+    <li>{{ item.original_language }}</li>
+    <li>{{ item.vote_average }}</li>
+</ul>
 </template>
 <style scoped lang="scss"></style>
