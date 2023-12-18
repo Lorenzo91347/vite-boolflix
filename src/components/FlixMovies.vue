b6813769f1b2514865233a7fae43c8290f4a203a
lorenzo91347 <lorenzo.losito91@gmail.com>
2023-12-18 13:03:16 +0100

    feat;milestone 2 fix

--- src/components/FlixMovies.vue
+++ src/components/FlixMovies.vue
@@ -15,8 +15,8 @@ export default{
 <ul v-for="item in store.movies">
     <li>{{ item.original_title }}</li>
     <li>{{ item.title }}</li>
-    <img v-if="item.original_language = 'en'" src="https://flagsapi.com/GB/flat/16.png">
-    <img v-else-if="item.original_language = 'it'" src="https://flagsapi.com/IT/flat/16.png">
+    <img v-if="item.original_language === 'en'" src="https://flagsapi.com/GB/flat/16.png">
+    <img v-else-if="item.original_language === 'it'" src="https://flagsapi.com/IT/flat/16.png">
     <li v-else>No data Available</li>
     <li>{{ item.vote_average }}</li>
 </ul>
@@ -24,8 +24,8 @@ export default{
 <ul v-for="item in store.tvSeries">
     <li>{{ item.original_name }}</li>
     <li>{{ item.name }}</li>
-    <img v-if="item.original_language = 'en'" src="https://flagsapi.com/GB/flat/16.png">
-    <img v-else-if="item.original_language = 'it'" src="https://flagsapi.com/IT/flat/16.png">
+    <img v-if="item.original_language === 'en'" src="https://flagsapi.com/GB/flat/16.png">
+    <img v-else-if="item.original_language === 'it'" src="https://flagsapi.com/IT/flat/16.png">
     <li v-else>No data Available</li>
     <li>{{ item.vote_average }}</li>
 </ul>
